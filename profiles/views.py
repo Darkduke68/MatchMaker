2b701024632176ffb4581a9c21c04eaedc924587
Darkduke68 <yuecao.1989@gmail.com>
2017-08-20 03:20:51 -0700

    profile page need to login to view

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from profiles.forms import ProfileUpdateForm
 
 
 # Create your views here.
+@login_required
 def profile_update(request):
     if request.method == 'POST':
         form = ProfileUpdateForm(request.POST, instance=request.user)
@@ -20,6 +23,7 @@ def profile_update(request):
         return render(request, 'profiles/update.html', context)
 
 
+@login_required
 def profile_get(request):
     if request.method == 'GET':
         return render(request, 'profiles/profile_card.html', {'user': request.user})
