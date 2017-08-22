5721e6a3bf2b326ff0b88be67a36c41a76c656ab
Darkduke68 <yuecao.1989@gmail.com>
2017-08-22 15:53:11 -0700

    update dashboard info

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from matches.models import Match
 
@@ -7,10 +8,11 @@ from matches.models import Match
 @login_required()
 def dashboard(request):
     """Endpoint to view user matches. """
-
+    total_user_cnt = User.objects.all().count() - 1
     matches = Match.objects.get_matches_with_percent(request.user)[:8]
     print(matches)
     context = {
         "matches": matches,
+        "total_user_cnt": total_user_cnt,
     }
     return render(request, "dashboards/main.html", context)
